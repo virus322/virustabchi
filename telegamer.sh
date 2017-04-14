abddb66f8743bb6559b55780d057270a749a8c1c
virus32 <iranonymous.virus32@gmail.com>
2017-04-14 18:40:33 +0430

    virustabchi

---
+++ telegamer.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+SCREENNUM=`ps -e | grep -c screen`
+TMUXNUM=`ps -e | grep -c tmux`
+Reloadtime=500
+clear
+f=3 b=4
+for j in f b; do
+  for i in {0..7}; do
+    printf -v $j$i %b "\e[${!j}${i}m"
+  done
+done
+rst=$'\e[0m'
+echo -e "\e[100m                                   Launcher Script                             \e[00;37;40m"
+echo -e "\e[01;34m                                    By Mehrpouya                               \e[00;37;40m"
+echo ""
+echo
+cat << EOF
+ $f1  ▀▄   ▄▀     $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4  ▀▄   ▄▀     $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
+ $f1 ▄█▀███▀█▄    $f2███▀▀██▀▀███   $f3▄█▀██▀█▄   $f4 ▄█▀███▀█▄    $f5███▀▀██▀▀███   $f6▄█▀██▀█▄$rst
+ $f1█▀███████▀█   $f2▀▀███▀▀███▀▀   $f3▀█▀██▀█▀   $f4█▀███████▀█   $f5▀▀███▀▀███▀▀   $f6▀█▀██▀█▀$rst
+ $f1▀ ▀▄▄ ▄▄▀ ▀   $f2 ▀█▄ ▀▀ ▄█▀    $f3▀▄    ▄▀   $f4▀ ▀▄▄ ▄▄▀ ▀   $f5 ▀█▄ ▀▀ ▄█▀    $f6▀▄    ▄▀$rst
+EOF
+sleep 1.5
+    echo -e "\e[01;34m Script Reload in $Reloadtime Seconds\e[00;37;40m"
+    echo -e "\e[01;34m number of screens running : $SCREENNUM\e[00;37;40m"
+    echo -e "\e[01;34m number of tmux running : $TMUXNUM\e[00;37;40m"
+cat << EOF
+ $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
+ $bld$f1█▄█▀███▀█▄█   $f2███▀▀██▀▀███   $f3▄█▀██▀█▄   $f4█▄█▀███▀█▄█   $f5███▀▀██▀▀███   $f6▄█▀██▀█▄$rst
+ $bld$f1▀█████████▀   $f2▀▀▀██▀▀██▀▀▀   $f3▀▀█▀▀█▀▀   $f4▀█████████▀   $f5▀▀▀██▀▀██▀▀▀   $f6▀▀█▀▀█▀▀$rst
+ $bld$f1 ▄▀     ▀▄    $f2▄▄▀▀ ▀▀ ▀▀▄▄   $f3▄▀▄▀▀▄▀▄   $f4 ▄▀     ▀▄    $f5▄▄▀▀ ▀▀ ▀▀▄▄   $f6▄▀▄▀▀▄▀▄$rst
+EOF
+while true ; do
+  for entr in tabchi-*.sh ; do
+    entry="${entr/.sh/}"
+    tmux kill-session -t $entry
+    tmux new-session -d -s $entry "./$entr"
+    tmux detach -s $entry
+  done
+  echo Bots Running!
+  
+  sleep $Reloadtime
+done
